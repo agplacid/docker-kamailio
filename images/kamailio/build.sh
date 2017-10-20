3c7241511fc08104a686590aa4a5066ff5218dae
Joe Black <me@joeblack.nyc>
2017-10-19 22:57:40 -0400

    bumped up memlog level to debug

--- images/kamailio/build.sh
+++ images/kamailio/build.sh
@@ -39,12 +39,17 @@ pushd $_
     popd && rm -rf $OLDPWD
 
 
+log::m-info "Applying fixes ..."
 # needed to fix fatal error module param doesn't exist
 sed -i '/cseq_offset/s/\(^.*$\)/# \1/' /etc/kamailio/presence-role.cfg
 
 # add LOG_LEVEL to local.cfg
 echo -e '
 #!define KAZOO_LOG_LEVEL <LOG_LEVEL>' >> /etc/kamailio/local.cfg
+
+# only print verbose memory stags on quit when at debug log level
+sed -i '/^memlog/s/L_INFO/L_DBG/' /etc/kamailio/default.cfg
+
 
 log::m-info "Removing curl ..."
 apt-get purge -y --auto-remove curl
